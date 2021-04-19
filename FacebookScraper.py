6b06311e418d7dd749b7bb5637d3d5747c750dbd
Jeffrey Qu <40805785+jqu-16@users.noreply.github.com>
2021-04-18 20:15:26 -0400

    Basic Scraping

--- FacebookScraper.py
+++ FacebookScraper.py
@@ -1,5 +1,80 @@
-from lxml import html
 import requests
+from bs4 import BeautifulSoup
+# from secrets import username, password
 
-page = requests.get('https://www.facebook.com/events/271388601195325/?active_tab=about')
-tree = html.fromstring(page.content)
+# page = requests.get('https://www.facebook.com/events/2842763029277309?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22discovery_top_tab%22%2C%22surface%22%3A%22bookmark%22%7D]%7D')
+# print(page.status_code)
+
+# soup = BeautifulSoup(page.content, 'html.parser')
+# # print(soup.prettify())
+
+# title = soup.find('title')
+# print(title.get_text())
+
+# elements = soup.find_all('div', class_='hidden_elem')
+# for element in elements:
+# 	print(element.get_text())
+
+class FaceBookBot():
+    login_basic_url = 'https://mbasic.facebook.com/login'
+    login_mobile_url = 'https://m.facebook.com/login'
+    payload = {
+            'email': 'qu.jeffrey@yahoo.com',
+            'pass': 'MQ8928489aa'
+        }
+    post_ID = ""
+
+    def parse_html(self, request_url):
+        with requests.Session() as session:
+            post = session.post(self.login_basic_url, data=self.payload)
+            parsed_html = session.get(request_url)
+        return parsed_html
+
+    def post_content(self):
+        REQUEST_URL = f'https://mbasic.facebook.com/story.php?story_fbid={self.post_ID}&id=415518858611168'
+        
+        soup = BeautifulSoup(self.parse_html(REQUEST_URL).content, "html.parser")
+        content = soup.find_all('p')
+        post_content = []
+        for lines in content:
+            post_content.append(lines.text)
+        
+        post_content = ' '.join(post_content)    
+        return post_content
+
+    def date_posted(self):
+        REQUEST_URL = f'https://mbasic.facebook.com/story.php?story_fbid={self.post_ID}&id=415518858611168'
+        
+        soup = BeautifulSoup(self.parse_html(REQUEST_URL).content, "html.parser")
+        date_posted = soup.find('abbr')
+        return date_posted.text
+
+    def post_likes(self):
+        limit = 200
+        REQUEST_URL = f'https://mbasic.facebook.com/ufi/reaction/profile/browser/fetch/?limit={limit}&total_count=17&ft_ent_identifier={self.post_ID}'
+
+        soup = BeautifulSoup(self.parse_html(REQUEST_URL).content, "html.parser")
+        names = soup.find_all('h3')
+        people_who_liked = []
+        for name in names:
+            people_who_liked.append(name.text)
+        people_who_liked = [i for i in people_who_liked if i] 
+        return people_who_liked
+
+    def post_shares(self):        
+        REQUEST_URL = f'https://m.facebook.com/browse/shares?id={self.post_ID}'
+        
+        with requests.Session() as session:
+            post = session.post(self.login_mobile_url, data=self.payload)
+            parsed_html = session.get(REQUEST_URL)
+        
+        soup = BeautifulSoup(parsed_html.content, "html.parser")
+        names = soup.find_all('span')
+        people_who_shared = []
+        for name in names:
+            people_who_shared.append(name.text)
+        return people_who_shared
+
+fb = FaceBookBot()
+html = fb.post_content()
+print(html)
